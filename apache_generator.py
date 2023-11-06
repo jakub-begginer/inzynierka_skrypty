b100c78c805cf0454d2b3225d91490af9fbfd001
jakub-begginer <110667020+jakub-begginer@users.noreply.github.com>
2023-11-06 12:08:18 +0100

    Update apache_generator.py
    Logs can be read by Elastic

--- apache_generator.py
+++ apache_generator.py
@@ -28,30 +28,60 @@ attack_paths = [
 status_codes = ['200', '404', '500']
 
 def generate_attack_log():
-    current_time = datetime.datetime.now().strftime('%d/%b/%Y:%H:%M:%S')
+    current_time = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
     remote_host = fake.ipv4()
     request_path = random.choice(attack_paths)
     http_status = random.choice(status_codes)
     user_agent = fake.user_agent()
-    
-    log_entry = f'{remote_host} - - [{current_time}] "GET {request_path} HTTP/1.1" {http_status} "-" "{user_agent}"'
+
+    log_entry = {
+        "@timestamp": current_time,
+        "clientip": remote_host,
+        "request": {
+            "method": "GET",
+            "url": request_path,
+            "httpversion": "1.1"
+        },
+        "response": {
+            "status": int(http_status),
+            "body_bytes": random.randint(100, 2048)
+        },
+        "user_agent": {
+            "original": user_agent
+        }
+    }
     return log_entry
 
 def generate_normal_log():
-    current_time = datetime.datetime.now().strftime('%d/%b/%Y:%H:%M:%S')
+    current_time = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
     remote_host = fake.ipv4()
     request_path = random.choice(['/page1', '/page2', '/page3', '/home', '/about'])
     http_status = random.choice(status_codes)
     user_agent = fake.user_agent()
-    
-    log_entry = f'{remote_host} - - [{current_time}] "GET {request_path} HTTP/1.1" {http_status} "-" "{user_agent}"'
+
+    log_entry = {
+        "@timestamp": current_time,
+        "clientip": remote_host,
+        "request": {
+            "method": "GET",
+            "url": request_path,
+            "httpversion": "1.1"
+        },
+        "response": {
+            "status": int(http_status),
+            "body_bytes": random.randint(100, 2048)
+        },
+        "user_agent": {
+            "original": user_agent
+        }
+    }
     return log_entry
 
 def save_logs(filename, num_logs, interval):
     while num_logs > 0:
         log_entry = generate_attack_log() if random.random() < 0.5 else generate_normal_log()
         with open(os.path.join(log_folder, filename), 'a') as file:
-            file.write(log_entry + '\n')
+            file.write(str(log_entry) + '\n')  # Zapisz log jako ciąg znaków JSON
         num_logs -= 1
         time.sleep(interval)
 
@@ -59,7 +89,7 @@ def main():
     parser = argparse.ArgumentParser(description='Generate Apache access logs with attack patterns.')
     parser.add_argument('--num-logs', type=int, default=100, help='Number of logs to generate')
     parser.add_argument('--interval', type=int, default=5, help='Interval in seconds between log generation')
-    parser.add_argument('--output-file', type=str, default='access_log', help='Output file name')
+    parser.add_argument('--output-file', type=str, default='access_log.json', help='Output file name')
     parser.add_argument('--log-folder', type=str, default=DEFAULT_LOG_FOLDER, help='Folder where logs will be saved')
     args = parser.parse_args()
 
